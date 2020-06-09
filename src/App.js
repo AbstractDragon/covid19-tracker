a16e3ebecb6504682c83b49d4c01a9ec48cb2763
Ayo ANIMASHAUN <ayofouad@gmail.com>
2020-06-09 10:36:24 +0100

    Made api request

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 import { Cards, Charts, CountryPicker } from './components';
 import styles from './App.module.css';
-
+import { fetchData } from './api/';
 class App extends React.Component {
+  state = {
+    data: {},
+  }
+
+  //use componentdidmount when making api requests
+  async componentDidMount(){
+    //use await when collecting data from an asynchrous function, inorder to use await it must be in an async function
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
         <h1>App</h1>
-        <Cards/>
+        <Cards data={data}/>
         <Charts/>
         <CountryPicker/>
       </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-const Cards = () => {
- return (
-     <h1>Cards</h1>
- )
+const Cards = (props) => {
+    console.log(props);
+    return (
+        <h1>Cards</h1>
+    )
 }
 
 export default Cards;
